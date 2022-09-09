a612ca1570dd98d7eafcaf0a5cd9f006507d7db9
Isaac Blanco <nuclearhands2001@gmail.com>
2022-09-08 21:15:57 -0500

    Creo que acabe

--- integration_test/authentication_test.dart
+++ integration_test/authentication_test.dart
@@ -14,18 +14,94 @@ void main() {
   testWidgets("Login sin creación de usuario", (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
+    await tester.tap(find.byKey(const Key('ButtonLoginSubmit')));
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
   });
 
   testWidgets("Login -> signup -> creación usuario -> login no exitoso",
       (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    // Login
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
+    await tester.tap(find.byKey(const Key('ButoonLoginCreateAccount')));
+    await tester.pumpAndSettle();
+
+    // Sign up
+    expect(find.byKey(const Key('SignUpPage')), findsOneWidget);
+
+    // Creación de usuario
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpEmail')), 'b@b.com');
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpPassword')), '321321');
+    await tester.tap(find.byKey(const Key('ButtonSignUpSubmit')));
+    await tester.pumpAndSettle();
+
+    // Login
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginEmail')), 'bb@.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonLoginSubmit')));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
   });
 
   testWidgets("Login -> signup -> creación usuario -> login exitoso -> logout",
       (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    // Login
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
+
+    await tester.tap(find.byKey(const Key('ButtonLoginCreateAccount')));
+
+    await tester.pumpAndSettle();
+
+    // Sign up
+    expect(find.byKey(const Key('SignUpPage')), findsOneWidget);
+
+    // Login
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonSignUpSubmit')));
+
+    await tester.pumpAndSettle();
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonLoginSubmit')));
+
+    await tester.pumpAndSettle();
+
+    // Login realizado correcto
+    expect(find.byKey(const Key('HomePage')), findsOneWidget);
+
+    // Log out
+    await tester.tap(find.byKey(const Key('ButtonHomeLogOff')));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('LoginScreen')), findsWidgets);
   });
 
   testWidgets(
@@ -33,6 +109,79 @@ void main() {
       (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    // Login
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
+
+    await tester.tap(find.byKey(const Key('ButtonLoginCreateAccount')));
+
+    await tester.pumpAndSettle();
+
+    // Sign up
+    expect(find.byKey(const Key('SignUpPage')), findsOneWidget);
+
+    // Login
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonSignUpSubmit')));
+
+    await tester.pumpAndSettle();
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonLoginSubmit')));
+
+    await tester.pumpAndSettle();
+
+    // Login realizado correcto
+    expect(find.byKey(const Key('HomePage')), findsOneWidget);
+
+    // Log out
+    await tester.tap(find.byKey(const Key('ButtonHomeLogOff')));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('LoginScreen')), findsWidgets);
+
+    // Login de nuevo
+    await tester.tap(find.byKey(const Key('ButtonLoginCreateAccount')));
+
+    await tester.pumpAndSettle();
+
+    // Sign up
+    expect(find.byKey(const Key('SignUpPage')), findsOneWidget);
+
+    // Login
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonSignUpSubmit')));
+
+    await tester.pumpAndSettle();
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonLoginSubmit')));
+
+    await tester.pumpAndSettle();
+
+    // Login realizado correcto
+    expect(find.byKey(const Key('HomePage')), findsOneWidget);
   });
 
   testWidgets(
@@ -40,5 +189,77 @@ void main() {
       (WidgetTester tester) async {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
+
+    // Login
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
+
+    await tester.tap(find.byKey(const Key('ButtonLoginCreateAccount')));
+
+    await tester.pumpAndSettle();
+
+    // Sign up
+    expect(find.byKey(const Key('SignUpPage')), findsOneWidget);
+
+    // Login
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonSignUpSubmit')));
+
+    await tester.pumpAndSettle();
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonLoginSubmit')));
+
+    await tester.pumpAndSettle();
+
+    // Login realizado correcto
+    expect(find.byKey(const Key('HomePage')), findsOneWidget);
+
+    // Log out
+    await tester.tap(find.byKey(const Key('ButtonHomeLogOff')));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(const Key('LoginScreen')), findsWidgets);
+
+    // Login de nuevo
+    await tester.tap(find.byKey(const Key('ButtonLoginCreateAccount')));
+
+    await tester.pumpAndSettle();
+
+    // Sign up
+    expect(find.byKey(const Key('SignUpPage')), findsOneWidget);
+
+    // Login
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldSignUpPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonSignUpSubmit')));
+
+    await tester.pumpAndSettle();
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginEmail')), 'b@b.com');
+
+    await tester.enterText(
+        find.byKey(const Key('TextFormFieldLoginPassword')), '321321');
+
+    await tester.tap(find.byKey(const Key('ButtonLoginSubmit')));
+
+    await tester.pumpAndSettle();
+
+    expect(find.text('User or passwor nok'), findsOneWidget);
   });
 }
